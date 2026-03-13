0dc54c6ffd099777ab39476c357aef9dde1328e9
chen <chen@altude.so>
2026-03-13 14:02:49 +0800

    update project.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,30 +5,8 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     //kotlin("jvm") version "2.2.0" apply false
-    kotlin("plugin.serialization") version "2.2.0" apply false
-    `maven-publish`
+    kotlin("plugin.serialization") version "2.2.0" apply false    
 }
-//val buildToolsVersion by extra("35.0.0")
-//
-//// Configure maven-publish for all Android library subprojects (required by JitPack)
-//subprojects {
-//    afterEvaluate {
-//        if (plugins.hasPlugin("com.android.library")) {
-//            apply(plugin = "maven-publish")
-//            extensions.configure<PublishingExtension> {
-//                publications {
-//                    create<MavenPublication>("release") {
-//                        groupId = project.findProperty("group")?.toString() ?: "com.github.AltudePlatform"
-//                        artifactId = project.name
-//                        version = project.findProperty("version")?.toString() ?: "1.0"
-//                        afterEvaluate {
-//                            from(components["release"])
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+
 
 
